fe92ba10c8abdad8d55587ec6191f9ef431001c2
lee <soo2n13@naver.com>
2020-09-18 10:54:23 +0900

    img,css 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './css/bootstrap.min.css';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>React</h1>
         <p>내 이름은 <strong>{myName}</strong></p>
         <img src={logo} />
